da58701874edb48d388c4dc217d7df4ea9abfdd8
rajashekarpodicheti <rajashekarpodicheti1@gmail.com>
2023-03-06 10:52:49 +0000

    main

---
+++ src/components/Home/index.js
@@ -0,0 +1,13 @@
+import BlogList from '../BlogList'
+import UserInfo from '../UserInfo'
+i
+import './index.css'
+
+const Home = () => (
+  <div className="home-container">
+    <UserInfo />
+    <BlogList />
+  </div>
+)
+
+export default Home
